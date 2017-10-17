16e57491224847010ad3bc80cd96a0d8aca1c9fe
sebasscoates@tufts.edu <sebastian.coates@tufts.edu>
2017-10-17 12:28:33 -0400

    Get location working with map

--- landmarks/script.js
+++ landmarks/script.js
@@ -4,16 +4,19 @@ function initializePage(){
 }
 
 function initMap(pos){
-        console.log(pos)
-        var coords = {lat: -25.363, lng: 131.044}
-        var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: coords 
-        });
+        if (pos!= undefined) {
+                console.log(pos)
+                u_coords = pos.coords;
+                var coords ={lat: u_coords.latitude, lng: u_coords.longitude} 
+                var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 12,
+                        center: coords 
+                });
 
-        var marker = new google.maps.Marker({
-            position: coords,
-            map: map,
-            title: 'Hello World!'
-        });
+                var marker = new google.maps.Marker({
+                    position: coords,
+                    map: map,
+                    title: 'Hello World!'
+                });
+        }
 }
