280d20d3b1307847ef54f5dfda5ddb987fd62667
sebasscoates@tufts.edu <sebastian.coates@tufts.edu>
2017-10-17 13:40:12 -0400

    server request working

--- landmarks/script.js
+++ landmarks/script.js
@@ -5,18 +5,32 @@ function initializePage(){
 
 function initMap(pos){
         if (pos!= undefined) {
-                console.log(pos)
                 u_coords = pos.coords;
                 var coords ={lat: u_coords.latitude, lng: u_coords.longitude} 
                 var map = new google.maps.Map(document.getElementById('map'), {
                         zoom: 12,
                         center: coords 
                 });
-
-                var marker = new google.maps.Marker({
-                    position: coords,
-                    map: map,
-                    title: 'Hello World!'
-                });
+                addMarkers(map, coords)
         }
+}
+
+function addMarkers(map, coords) {
+        var r = new XMLHttpRequest()
+        sendstring = "login=CCqBXEWf" + "&lat=" + coords.lat + "&lng=" + coords.lng
+        r.open("POST","https://defense-in-derpth.herokuapp.com/sendLocation" ,true)
+        r.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+
+        r.onreadystatechange = function() {
+                if (r.readyState == 4 && r.status == 200) {
+                
+                } 
+        }; 
+        r.send(sendstring)
+        
+        var marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            title: 'Hello World!'
+        });
 }
