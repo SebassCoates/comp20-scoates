7918bafe8ab0acbea5248447775f3f00f340bf0c
sebasscoates@tufts.edu <sebastian.coates@tufts.edu>
2017-10-17 10:26:22 -0400

    gets location

--- landmarks/script.js
+++ landmarks/script.js
@@ -1,9 +1,11 @@
-function initializePage() {
-
+function initializePage(){ 
+        navigator.geolocation.getCurrentPosition(initMap)
+        initMap()
 }
 
-function initMap(){
-        var coords = {lat: -25.363, lng: 131.044};
+function initMap(pos){
+        console.log(pos)
+        var coords = {lat: -25.363, lng: 131.044}
         var map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 4,
                 center: coords 
